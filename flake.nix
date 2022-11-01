60fcc0f46b0141b74d109e1ed0dd487930041366
aabccd021 <aabccd021@gmail.com>
2022-11-01 08:44:08 +0700

    fix: assert version

--- flake.nix
+++ flake.nix
@@ -13,8 +13,16 @@
       shellHook = ''
         export PLAYWRIGHT_BROWSERS_PATH=${playwright.browsers}
         export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
-        [ "$(nix --version)" == "nix (Nix) 2.11.0" ] || exit 1
-        [ "$(direnv --version)" == "2.32.1" ] || exit 1
+        function assertEqual {
+          if [[ "$1" != "$2" ]]; then
+            echo -e "\033[0;31mInvalid version"
+            echo -e "\033[0;31mActual: $1"
+            echo -e "\033[0;31mExpected: $2"
+            exit 1
+          fi
+        }
+        assertEqual "$(nix --version)" "nix (Nix) 2.11.0" || exit 1
+        assertEqual "$(direnv --version)" "2.2.1" || exit 1
       '';
     };
   };
